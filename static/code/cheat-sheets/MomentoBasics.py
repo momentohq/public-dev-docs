8baef2467c0ff5f4aa63c1406108c99379504fce
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-03-21 11:53:44 -0700

    Updated cheat sheet and added entire file
    Updated the cheat sheet to change name of class and added entire class file to the /static directory

--- static/code/cheat-sheets/MomentoBasics.py
+++ static/code/cheat-sheets/MomentoBasics.py
@@ -1,23 +1,9 @@
----
-sidebar_position: 1
-sidebar_label: Python
-sidebar_class_name: sidebar-item-python-cheat-sheet
-title: Cheat Sheet for Python + Momento Serverless Cache
-description: Everything to get you going with coding using Python and Momento Serverless Cache
----
-
-# Cheat Sheet for Python with Momento Serverless Cache
-If you need to get going quickly with Python and Momento Serverless Cache, here are the basic API calls you'll need. When you need them, check out the [API reference page](../../api-reference/index.mdx) for more information on the full assortment of Momento API calls.
-
-If you combine all of the functions on this page into one python file, you'd have a central collection of functions you can import and call from other python code. In addition, if you are putting this into production, you might look to replace the print() calls with ones using the logging library (`import logging`) in python.
-
-## Import libraries and connect to return a CacheClient object
-```python
 from datetime import timedelta
 from momento import CacheClient, Configurations, CredentialProvider
 from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches, CacheIncrement
 
 import os
+
 class MomentoBasics:
   def client():
     momento_auth_token = CredentialProvider.from_environment_variable('MOMENTO_AUTH_TOKEN')
@@ -28,11 +14,7 @@ class MomentoBasics:
       'default_ttl':  ttl
     }
     return CacheClient(**config)
-```
 
-## Create a new cache in Momento Serverless Cache
-Use this function to create a new cache in your account
-```python
   def create_cache(cache_name) -> None:
     with MomentoBasics.client() as client:
       resp = client.create_cache(cache_name)
@@ -43,11 +25,7 @@ Use this function to create a new cache in your account
             print(f"Error incrementing value: {error.message}")
         case _ as error:
             print(f"Unreachable with {error.message}")
-```
 
-## Get list of existing caches in your account
-In this example, we use the client function above to get a client object and then use it to list all of the cache's for this account.
-```python
   def list_caches() -> None:
       print("Listing caches:")
       with MomentoBasics.client() as client:
@@ -61,10 +39,7 @@ In this example, we use the client function above to get a client object and the
             case _:
                 print("Unreachable")
         print("")
-```
-## Write an item to a cache
-A simple example of doing a set operation. In the client.set call, you could pass a fourth value, the TTL and this would override the default TTL value set with the client connection object.
-```python
+
   def set(cache_name, key, value, ttl=None):
     with MomentoBasics.client() as client:
       resp = client.set(cache_name, key, value, ttl)
@@ -76,9 +51,6 @@ A simple example of doing a set operation. In the client.set call, you could pas
         case _:
             print("Unreachable")
 
-## Read an item from a cache
-This is an example of a simple read operation to get an item from a cache.
-```python
   def get(cache_name, key):
     with MomentoBasics.client() as client:
       resp = client.get(cache_name, key)
@@ -88,10 +60,7 @@ This is an example of a simple read operation to get an item from a cache.
         raise ValueError("There is a cache miss. That key does not exist in this cache.", cache_name, "key_name : " + key)
       elif isinstance(resp, CacheGet.Error):
         raise resp.inner_exception
-```
-## Increment the value of an item in a cache
-An example of incrementing a value in a key. You can pass in any number
-```python
+      
   def incr(cache_name, key, value:int = 1):
     with MomentoBasics.client() as client:
       resp = client.increment(cache_name, key, value)
@@ -102,14 +71,3 @@ An example of incrementing a value in a key. You can pass in any number
             print(f"Error incrementing value: {error.message}")
         case _:
             print("Unreachable")
-```
-
-Click [here](../../../../static/code/cheat-sheets/MomentoBasics.py) to see the class file with all definitions in it.
-
-## Notes of further usage
-It could be adventageous to reuse the CacheClient connection object across multiple API calls depending on how your app works. If this is something you want to do, then you should modify the client() function definition to return a CacheClient object like so:
-```python
-def client() -> CacheClient:
-```
-
-The modify the other function definitions to accept a CacheClient object as an argument, but also not instantiate its own object.
