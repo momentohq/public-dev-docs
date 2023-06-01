317365618a6baa820388740538141a664a521c89
pgautier404 <pgautier404@users.noreply.github.com>
2023-06-01 12:02:42 -0700

    feat: adding Python docs (#249)
    * feat: adding Python docs

---
+++ plugins/example-code-snippets/src/examples/resolvers/source-parsers/languages/python-snippet-source-parser.ts
@@ -0,0 +1,35 @@
+import {
+  RegexSnippetSourceParser,
+  RegexSnippetTypeOptions,
+} from '../regex-snippet-source-parser';
+import {ExampleSnippetType} from '../../../examples';
+import * as path from 'path';
+
+export class PythonSnippetSourceParser extends RegexSnippetSourceParser {
+  constructor(repoSourceDir: string) {
+    const wholeFileExamplesDir = 'examples/py310/doc-example-files';
+    const codeSnippetFiles: Array<string> = [
+      'examples/py310/doc-examples-python-apis.py',
+    ];
+    super({
+      wholeFileExamplesDir: path.join(repoSourceDir, wholeFileExamplesDir),
+      snippetTypeParseOptions: new Map<
+        ExampleSnippetType,
+        RegexSnippetTypeOptions
+      >([
+        [
+          ExampleSnippetType.CODE,
+          {
+            snippetSourceFiles: codeSnippetFiles.map(f =>
+              path.join(repoSourceDir, f)
+            ),
+            startRegex: snippetId =>
+              new RegExp(`^(?:async )?def example_${snippetId.valueOf()}\\(`),
+            endRegex: () => /# end example/m,
+            numLeadingSpacesToStrip: 4,
+          },
+        ],
+      ]),
+    });
+  }
+}
